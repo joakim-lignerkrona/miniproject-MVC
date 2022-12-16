17f80d6822f377303df7eaf62fe47f2b5bd1f87b
Joakim <jockan@icloud.com>
2022-12-16 12:26:25 +0100

    Scaffoldning command

--- MiniprojectDB/ConsultantRandomizer/Tables/People.sql
+++ MiniprojectDB/ConsultantRandomizer/Tables/People.sql
@@ -1,4 +1,7 @@
 ﻿CREATE TABLE [dbo].[People]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [DiscordID] NCHAR(200) NOT NULL, 
+    [DisplayName] NCHAR(20) NOT NULL, 
+    [CustomDisplayName] NCHAR(10) NULL
 )
