f26c1bc8642f417d946fdf5455e94d948dc6cdcf
Joakim <jockan@icloud.com>
2022-12-16 17:07:57 +0100

    login working

--- miniprojekt-MVC/Controllers/AdminController.cs
+++ miniprojekt-MVC/Controllers/AdminController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace miniprojekt_MVC.Controllers
 {
     public class AdminController : Controller
     {
         [HttpGet("/admin")]
+        [Authorize]
         public IActionResult Index()
         {
             return View();
