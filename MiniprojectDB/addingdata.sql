bd3ee29ec23e908f87d7017f1b617a444950c868
Joakim Lignerkrona <jockan@icloud.com>
2022-12-19 11:00:54 +0100

    data

---
+++ MiniprojectDB/addingdata.sql
@@ -0,0 +1,19 @@
+﻿insert into [ConsultantRandomizer].[Group](name)
+values
+('Hejlsberg'),
+('Eich'),
+('Gates'),
+('Turing'),
+('Torvalds'),
+('Gosling'),
+('Lovelace'),
+('Liskov'),
+('Hopper'),
+('Shaw'),
+('Keller')
+
+insert into [ConsultantRandomizer].[People](DisplayName)
+Values 
+('Jockan'),
+('Raveforte'),
+('Square Hammer')
